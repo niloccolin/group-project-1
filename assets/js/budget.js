a848ccb5c20042533f583449c56b143182db01f6
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-07-14 12:01:07 -0400

    worked more on the dashboard list entries.

--- assets/js/budget.js
+++ assets/js/budget.js
@@ -74,7 +74,7 @@ addIncome.addEventListener("click", function(){
 incomeList.addEventListener("click", deleteOrEdit);
 expenseList.addEventListener("click", deleteOrEdit);
 allList.addEventListener("click", deleteOrEdit);
-//// EDIT AND DELETE BUTTONS  ///////
+//// EDIT AND DELETE BUTTONS  ////////
 function deleteOrEdit(event){
     const targetBtn = event.target;
     const entry = targetBtn.parentNode;
@@ -122,12 +122,12 @@ function updateUI(){
     });
     localStorage.setItem("entry_list", JSON.stringify(ENTRY_LIST));
 }
-////////// ADD HTML/BUTTONS FOR LIST ELEMENTS ////////////
+////// ADD HTML/BUTTONS FOR LIST ELEMENTS ///////
 function showEntry(list, type, title, amount, id){
     const entry = ` <li id = "${id}" class="${type}">
-                        <div class="entry">${title}: $${amount}
+                        <div class="entry">${title}: $${amount}</div>
                         <button id="edit">edit</button>
-                        <button id="delete">delete</button></div>
+                        <button id="delete">delete</button>
                     </li>`;
 
     const position = "afterbegin";
@@ -139,7 +139,7 @@ function clearElement(elements){
         element.innerHTML = "";
     })
 }
-////// MATH FOR ENTRIES ///////
+///// MATH FOR ENTRIES //////
 function calculateTotal(type, list){
     let sum = 0;
 
@@ -149,17 +149,17 @@ function calculateTotal(type, list){
         }
     })
     return sum;
- //// FUNCTION TO CALCULATE BALANCE
 }
+//// FUNCTION TO CALCULATE BALANCE  ////
 function calculateBalance(income, outcome){
     return income - outcome;
 }
-//// FUNCTIONS TO SHOW/HIDE LISTS
 function clearInput(inputs){
     inputs.forEach( input => {
         input.value = "";
     })
 }
+/// FUNCTIONS TO SHOW/HIDE LISTS ///
 function show(element){
     element.classList.remove("hide");
 }
